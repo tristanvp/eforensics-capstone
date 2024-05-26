aef594e5a434ea51249402e33b9485822100d104
Hong Quan Ngo <ngohongquan24462@gmail.com>
2024-05-26 11:05:57 +1000

    setting up base classes for the system

---
+++ backend/modules/base/ewf_image.py
@@ -0,0 +1,16 @@
+import pytsk3
+
+class EWFImgInfo(pytsk3.Img_Info):
+    def __init__(self, ewf_handle):
+        self._ewf_handle = ewf_handle
+        super(EWFImgInfo, self).__init__(url="", type=pytsk3.TSK_IMG_TYPE_EXTERNAL)
+
+    def close(self):
+        self._ewf_handle.close()
+
+    def read(self, offset, size):
+        self._ewf_handle.seek(offset)
+        return self._ewf_handle.read(size)
+    
+    def get_size(self):
+        return self._ewf_handle.get_media_size()
