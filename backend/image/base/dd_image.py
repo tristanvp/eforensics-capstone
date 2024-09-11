dae02e264ea49c9416c9f6d7e5d85d659b533497
jc <103074202@student.swin.edu.au>
2024-09-11 11:26:17 +1000

    Update dd_image.py
    Partion logic for DD and EWF files

--- backend/image/base/dd_image.py
+++ backend/image/base/dd_image.py
@@ -1,11 +1,33 @@
 import pytsk3
 import os
 
+# DDImage base class
+# Added partion logic to abstract away from parent, 
+
 class DDImgInfo(pytsk3.Img_Info):
     def __init__(self, dd_handle):
         self._dd_handle = dd_handle
         super(DDImgInfo, self).__init__(url="", type=pytsk3.TSK_IMG_TYPE_EXTERNAL)
 
+    def _parse_partitions(self):
+        #Detect and return a list of partitions for the raw DD image.
+        partition_table = pytsk3.Volume_Info(self)
+        partitions = []
+        for partition in partition_table:
+            if partition.len > 0:  # Valid partition
+                partitions.append({
+                    "index": partition.addr,
+                    "start": partition.start,
+                    "length": partition.len,
+                    "description": partition.desc.decode('utf-8'),
+                })
+
+        return partitions
+    
+    def get_partitions(self):
+        # Return partition data 
+        return self.partitions
+    
     def close(self):
         self._dd_file.close()
 
@@ -14,4 +36,4 @@ class DDImgInfo(pytsk3.Img_Info):
         return self._dd_file.read(size)
     
     def get_size(self):
-        return self._dd_file.get_media_size() 
+        return os.path.getsize(self._dd_handle.name) 
