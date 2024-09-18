5ecc93a1091eab6778d3463e3e96288024ba89bc
jc <103074202@student.swin.edu.au>
2024-09-18 12:02:57 +1000

    update to method calls to match filesystem

--- backend/file_analysis/sus_files_discovery.py
+++ backend/file_analysis/sus_files_discovery.py
@@ -1,86 +1,39 @@
+from backend.utility.filesystem import FileSystem
 import os
-import hashlib
-import time
 import struct
 import datetime
-from backend.utility.filesystem import FileSystem  
 
 class SusFilesDiscovery:
     def __init__(self, fs: FileSystem):
         self.fs = fs
-        self.suspicious_types = ['.exe', '.config', '.db', '.log', '.sqlite', '.sh']
         self.suspicious_files = []
+        self.suspicious_types = ['.exe', '.dll', '.bat', '.ps1', '.sh']  # Example suspicious types
 
     def run(self):
-        # Main method to run file discovery methods
-        suspicious_files = self.list_suspicious_files()
-        recycle_bin_files = self.recurse_dollar_i_files()  # Process $I and $R files in the Recycle Bin
-        return suspicious_files + recycle_bin_files
-
-    def list_suspicious_files(self):
-        # Search for files with suspicious extensions
-        self.recurse_directory()
+        self.recurse_interesting_extension_files()
+        self.recurse_dollar_i_files()
         return self.suspicious_files
 
-    def recurse_directory(self):
-        # Recurse through file system and find files with suspicious extensions
-        file_number = 1
-        for fs_object in self.fs.recurse_files(path='/'):
-            if fs_object.info.name.name.decode('utf-8').lower().endswith(tuple(self.suspicious_types)):
-                file_properties = self.get_file_properties(fs_object, file_number)
-                if file_properties:
-                    self.suspicious_files.append(file_properties)
-                    file_number += 1
-
-    def get_file_properties(self, fs_object, file_number):
-        # Return file properties including MD5 hash
-        try:
-            file_properties = {
-                "no": file_number,
-                "name": fs_object.info.name.name.decode('utf-8'),
-                "path": fs_object.info.meta.addr,  # Inode number as a substitute for path in a disk image
-                "created": time.ctime(fs_object.info.meta.crtime),
-                "accessed": time.ctime(fs_object.info.meta.atime),
-                "modified": time.ctime(fs_object.info.meta.mtime),
-                "bytes": fs_object.info.meta.size,
-                "md5": None
-            }
-
-            # MD5 hash calculation
-            md5_hash = hashlib.md5()
-            file_size = fs_object.info.meta.size
-
-            if file_size > 0:
-                file_content = fs_object.read_random(0, file_size)
-                md5_hash.update(file_content)
-                file_properties["md5"] = md5_hash.hexdigest()
-
-            return file_properties
-        except Exception as e:
-            print(f"Error retrieving properties for file: {fs_object.info.name.name.decode('utf-8')}, Error: {str(e)}")
-            return None
+    def recurse_interesting_extension_files(self):
+        interesting_file_extensions = ['.config', '.exe', '.db', '.sqlite', '.sqlite3', '.log', '.xml', '.tmp', '.dmp', '.txt', '.sh']
+        for ext in interesting_file_extensions:
+            interesting_files = self.fs.recurse_files(substring=ext, path='/', logic='endswith', case=False)
+            if interesting_files:
+                self.suspicious_files.extend(interesting_files)
 
     def recurse_dollar_i_files(self):
-        # Search for $I files in the Recycle Bin
-        dollar_i_files = self.fs.recurse_files("$I", path='/$Recycle.bin', logic="startswith")
-        if dollar_i_files is not None:
-            return self.process_dollar_i(dollar_i_files)
-        else:
-            print("No $I files found")
+        dollar_i_files = self.fs.recurse_files(substring='$I', path='/$Recycle.bin', logic='startswith', case=False)
+        if dollar_i_files:
+            self.process_dollar_i(dollar_i_files)
 
-    def process_dollar_i(self, dollar_i_files: list):
-        # Process the found $I files
-        processed_files = []
+    def process_dollar_i(self, dollar_i_files):
         for dollar_i in dollar_i_files:
-            file_attribs = self.validate_dollar_i(dollar_i[3])  # Validate $I file structure
+            file_attribs = self.validate_dollar_i(dollar_i[3])
             if file_attribs is None:
-                continue  # Invalid $I file, skip to the next one
+                continue
             file_attribs['dollar_i_file'] = os.path.join('/$Recycle.bin', dollar_i[2][1:])
-            
-            # Get the $R file path
             recycle_file_path = os.path.join('/$Recycle.bin', dollar_i[1].rsplit("/", 1)[0][1:])
-            dollar_r_files = self.fs.recurse_files("$R" + dollar_i[0][2:], path=recycle_file_path, logic="startswith")
-            
+            dollar_r_files = self.fs.recurse_files(substring='$R' + dollar_i[0][2:], path=recycle_file_path, logic='startswith', case=False)
             if dollar_r_files is None:
                 dollar_r_dir = os.path.join(recycle_file_path, "$R" + dollar_i[1][2:])
                 dollar_r_dirs = self.fs.query_directory(dollar_r_dir)
@@ -94,12 +47,9 @@ class SusFilesDiscovery:
                 dollar_r = [os.path.join(recycle_file_path, r[1][1:]) for r in dollar_r_files]
                 file_attribs['dollar_r_file'] = ";".join(dollar_r)
                 file_attribs['is_directory'] = False
-            
-            processed_files.append(file_attribs)
-        return processed_files
+            self.suspicious_files.append(file_attribs)
 
     def sizeof_fmt(self, num, suffix='B'):
-        # Format file sizes into human-readable form (e.g., KB, MB)
         for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
             if abs(num) < 1024.0:
                 return "%3.1f%s%s" % (num, unit, suffix)
@@ -107,18 +57,16 @@ class SusFilesDiscovery:
         return "%.1f%s%s" % (num, 'Yi', suffix)
 
     def parse_windows_filetime(self, date_value):
-        # Convert Windows filetime format into human-readable date
         microseconds = float(date_value) / 10
         ts = datetime.datetime(1601, 1, 1) + datetime.timedelta(microseconds=microseconds)
         return ts.strftime('%Y-%m-%d %H:%M:%S.%f')
 
     def validate_dollar_i(self, file_obj):
-        # Validate and extract attributes from $I file
-        if file_obj.read_random(0, 8) != '\x01\x00\x00\x00\x00\x00\x00\x00':
-            return None  # Invalid $I file
+        if file_obj.read_random(0, 8) != b'\x01\x00\x00\x00\x00\x00\x00\x00':
+            return None
         raw_file_size = struct.unpack('<q', file_obj.read_random(8, 8))
         raw_deleted_time = struct.unpack('<q', file_obj.read_random(16, 8))
-        raw_file_path = file_obj.read_random(24, 520)  # Extract file path (before Windows 10 format)
+        raw_file_path = file_obj.read_random(24, 520)
         file_size = self.sizeof_fmt(raw_file_size[0])
         deleted_time = self.parse_windows_filetime(raw_deleted_time[0])
         file_path = raw_file_path.decode("utf16").strip("\x00")
