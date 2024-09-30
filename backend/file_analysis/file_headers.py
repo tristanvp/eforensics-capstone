1119edc880c6702d005dc09f18c44e345dc22423
Ronan-Mc <163089921+Ronan-Mc@users.noreply.github.com>
2024-09-30 19:49:31 +1000

    Added more functionality to file_header.py

--- backend/file_analysis/file_headers.py
+++ backend/file_analysis/file_headers.py
@@ -12,9 +12,6 @@ class fileHeader:
         self.filepath = r"" #Sample filepath for finding the image file
         self.file_header = "" #Empty string for the file header to be found
 
-    def run(self, filepath):
-        self.find_file(filepath) #runs the code to find the file header
-
     def jpeg_file(self, data):
         header = b'\xFF\xD8\xFF\xE0' #define header for a JPEG file
         if data.find(header) != -1: #searches the data for the jpeg header, if it is not found, output is -1, so if output does not equal -1, the jpeg header was found in the data
@@ -24,21 +21,43 @@ class fileHeader:
 
     def pdf_file(self, data):
         header = b'%PDF' #define header for a PDF file
-        if data.find(header) != -1: #same as jpeg, but with the PDF header
+        if data.find(header) != -1: #same as above, but with the PDF header
             self.file_header = "PDF"
         else:
             self.zip_file(data)
 
     def zip_file(self, data):
         header = b'\x50\x4B\x03\x04' #define header for zip file
-        if data.find(header) != -1: #same as jpeg, but with the ZIP header
+        if data.find(header) != -1: #same as above, but with the ZIP header
             self.file_header = "ZIP"
+        else:
+            self.rtf_file(data) #if the header is not found, check for log file header
+
+    def rtf_file(self, data):
+        header = b'\7B\x5C\x72\x74\x66\x31' #define header for rtf file
+        if data.find(header) != -1: #same as above, but with the rtf header
+            self.file_header = "RTF"
+        else:
+            self.dat_file(data) #if the header is not found, search for dat file header
+
+    def dat_file(self, data):
+        header = b'\x72\x65\x67\x66' #define header for dat file
+        if data.find(header) != -1: #same as above, but with the dat header
+            self.file_header = "DAT"
+        else:
+            self.dll_file(data) #if the header is not found, search for dll file header
+    
+    def dll_file(self, data):
+        header = b'\x4D\x5A' #define header for dll file
+        if data.find(header) != -1: #same as above, but with the dll header
+            self.file_header = "DLL"
         else:
             self.file_header = "No valid file header found" #if the header is not found, no valid header is detected so it returns the default file header message
 
-    def find_file(self, filepath):
+    def run(self, filepath):
         with open(filepath, 'rb') as f: #opens the file at the example filepath
             data = f.read() #reads the file and inputs into the variable data
         self.jpeg_file(data) #Begins checking if data has a Jpeg header
-      
-    return self.file_header #return the file header for the rest of the system
+    
+    def get_file_header(self):
+        return self.file_header #return the file header for the rest of the system
